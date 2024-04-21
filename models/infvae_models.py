Fix: map enabled since used in loss calculation

models/infvae_models.py
@@ -289,11 +289,11 @@ class InfVAECascades(Model):
             #     tf.compat.v1.py_func(mean_recall_at_k, [self.relevance_scores, k, m],
             #                          tf.float32) for k in self.k_list
             # ]
-            #
-            # self.map_scores = [
-            #     tf.compat.v1.py_func(MAP, [self.relevance_scores, k, m], tf.float32)
-            #     for k in self.k_list
-            # ]
+
+            self.map_scores = [
+                tf.compat.v1.py_func(MAP, [self.relevance_scores, k, m], tf.float32)
+                for k in self.k_list
+            ]
 
             self.f1_scores = [
                 tf.compat.v1.py_func(mean_f1_at_k, [self.relevance_scores, k, m], tf.float32)
@@ -316,7 +316,7 @@ class InfVAECascades(Model):
             # ]
 
             self.recall_scores = tf.zeros((1, len(self.k_list)))
-            self.map_scores = tf.zeros((1, len(self.k_list)))
+            # self.map_scores = tf.zeros((1, len(self.k_list)))
             self.fpr_scores = tf.zeros((1, len(self.roc_k_list)))
             self.tpr_scores = tf.zeros((1, len(self.roc_k_list)))
             # self.f1_scores = tf.zeros((1, len(self.f1_k_list)))

train.py
@@ -184,7 +184,7 @@ def main(**kwargs):
                 num_eval_k_roc = len(CoAtt.roc_k_list)  # ROC curve: FPR over TPR
                 avg_map_scores, avg_recall_scores = [0.] * num_eval_k, [0.] * num_eval_k
                 avg_f1_scores = [0.] * num_eval_k_f1
-                avg_fpr_scores, avg_tpr_scores = [0.] * num_eval_k_roc, [0.] * num_eval_k_roc
+                # avg_fpr_scores, avg_tpr_scores = [0.] * num_eval_k_roc, [0.] * num_eval_k_roc
 
                 all_outputs, all_targets = [], []
                 for b in range(0, CoAtt.num_test_batches):
@@ -195,28 +195,28 @@ def main(**kwargs):
                     avg_map_scores = list(
                         map(operator.add, map(operator.mul, maps,
                                               [num_samples] * num_eval_k), avg_map_scores))
-                    avg_recall_scores = list(map(operator.add, map(operator.mul, recalls,
-                                                                   [num_samples] * num_eval_k), avg_recall_scores))
+                    # avg_recall_scores = list(map(operator.add, map(operator.mul, recalls,
+                    #                                                [num_samples] * num_eval_k), avg_recall_scores))
                     avg_f1_scores = list(map(operator.add, map(operator.mul, f1s,
                                                                [num_samples] * num_eval_k_f1), avg_f1_scores))
-                    avg_fpr_scores = list(map(operator.add, map(operator.mul, fprs,
-                                                                [num_samples] * num_eval_k_roc), avg_fpr_scores))
-                    avg_tpr_scores = list(map(operator.add, map(operator.mul, tprs,
-                                                                [num_samples] * num_eval_k_roc), avg_tpr_scores))
+                    # avg_fpr_scores = list(map(operator.add, map(operator.mul, fprs,
+                    #                                             [num_samples] * num_eval_k_roc), avg_fpr_scores))
+                    # avg_tpr_scores = list(map(operator.add, map(operator.mul, tprs,
+                    #                                             [num_samples] * num_eval_k_roc), avg_tpr_scores))
                     total_samples += num_samples
                 all_outputs = np.vstack(all_outputs)
                 all_targets = np.vstack(all_targets)
                 avg_map_scores = list(map(operator.truediv, avg_map_scores, [total_samples] * num_eval_k))
-                avg_recall_scores = list(map(operator.truediv, avg_recall_scores, [total_samples] * num_eval_k))
+                # avg_recall_scores = list(map(operator.truediv, avg_recall_scores, [total_samples] * num_eval_k))
                 avg_f1_scores = list(map(operator.truediv, avg_f1_scores, [total_samples] * num_eval_k_f1))
-                avg_fpr_scores = list(map(operator.truediv, avg_fpr_scores, [total_samples] * num_eval_k_roc))
-                avg_tpr_scores = list(map(operator.truediv, avg_tpr_scores, [total_samples] * num_eval_k_roc))
+                # avg_fpr_scores = list(map(operator.truediv, avg_fpr_scores, [total_samples] * num_eval_k_roc))
+                # avg_tpr_scores = list(map(operator.truediv, avg_tpr_scores, [total_samples] * num_eval_k_roc))
 
                 metrics = dict()
                 for k in range(0, num_eval_k):
                     K = CoAtt.k_list[k]
                     metrics[f"MAP@{K:0>3}"] = avg_map_scores[k]
-                    metrics[f"Recall@{K:0>3}"] = avg_recall_scores[k]
+                #     metrics[f"Recall@{K:0>3}"] = avg_recall_scores[k]
                 for k in range(0, num_eval_k_f1):
                     K = CoAtt.f1_k_list[k]
                     metrics[f"F1@{K:0>3}"] = avg_f1_scores[k]